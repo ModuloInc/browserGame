446c42c03f5295c3bf183fcc1a98b82ba70992eb
pacardflorian <pacardflo02@gmail.com>
2025-04-08 14:44:15 +0200

    load balencer

---
+++ scripts/start_servers.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+node server/js/main.js --config=server/config_8001.json &
+node server/js/main.js --config=server/config_8002.json &
+node server/js/main.js --config=server/config_8003.json &
+node server/js/main.js --config=server/config_8004.json &
+node server/js/main.js --config=server/config_8005.json &
